8a1c5b45ee089bb10b175cb82b0ce17becae97a7
Chienhm <54258170+chienhm@users.noreply.github.com>
2026-01-20 23:01:34 +0700

    Update button labels for import and clear actions

--- AIAnalyzer.py
+++ AIAnalyzer.py
@@ -1387,14 +1387,14 @@ class BurpExtender(IBurpExtender, IHttpListener, ITab, IContextMenuFactory):
 
         # Import Button
         thc.gridx = 2
-        btn_import = JButton("Import")
+        btn_import = JButton("Import Sitemap")
         btn_import.setMargin(Insets(2, 5, 2, 5))
         btn_import.addActionListener(self.import_scan_data)
         tree_header.add(btn_import, thc)
         
         # Clear Button
         thc.gridx = 3
-        btn_clean = JButton("Clear")
+        btn_clean = JButton("Clear Sitemap")
         btn_clean.setMargin(Insets(2, 5, 2, 5))
         btn_clean.addActionListener(self._action_clear_sitemap)
         tree_header.add(btn_clean, thc)
